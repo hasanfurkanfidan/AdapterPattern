1387fcd63eb347869dccd2181f9ca9b4ce09cba2
Hasan Furkan Fidan <furkanfidan.job@gmail.com>
2022-07-07 13:31:41 +0300

    Add project files.

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager userManager = new UserManager(new UserAnotherServiceAdapter());
+            userManager.GetUsers();
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Projenin ana servisi
+        /// </summary>
+        class UserManager
+        {
+            UserConnection _userConnection;
+            public UserManager(UserConnection userConnection)
+            {
+                _userConnection = userConnection;
+            }
+            public void GetUsers()
+            {
+                _userConnection.GetUsers();
+            }
+        }
+
+        /// <summary>
+        /// Kullanıcıları getirmek üzere çalışacak metodun imzası
+        /// </summary>
+        interface UserConnection
+        {
+            void GetUsers();
+        }
+
+        /// <summary>
+        /// Kullanıcıların db den geleceği fonksiyon
+        /// </summary>
+        class UserDbConnection : UserConnection
+        {
+            public void GetUsers()
+            {
+                Console.Write("Users brought from db");
+            }
+        }
+
+        /// <summary>
+        /// Kullanıcıları getirirken farklı bir sisteme adapte olunacak kodların yazıldığı class
+        /// </summary>
+        class UserAnotherServiceAdapter : UserConnection
+        {
+            AnotherService anotherService = new AnotherService();
+            public void GetUsers()
+            {
+                anotherService.GetUsersFromAnotherService();
+            }
+        }
+
+        /// <summary>
+        /// Farklı sistem
+        /// </summary>
+        class AnotherService
+        {
+            public void GetUsersFromAnotherService()
+            {
+                Console.Write("Users brought from Another Service");
+            }
+        }
+    }
+}
